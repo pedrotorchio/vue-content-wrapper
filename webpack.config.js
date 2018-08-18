d3d9c2939b26f8989caeb1da7dbde71cb5786a2e
pedrotorchio <pedrotorchio@gmail.com>
2018-08-18 12:21:17 -0300

    renaming entry

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/ContentWrapper',
+  entry: './src/ContentWrapper.vue',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
